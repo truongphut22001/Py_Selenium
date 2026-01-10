5d6f1c08640e7db7754a5f590f85ee453dacafb7
truongphut22001 <truongphut22001@gmail.com>
2026-01-10 16:04:42 +0700

    Script 47_50_Implicit_Explicit_Wait

---
+++ 47_50.Implicit_Explicit_Wait.py
@@ -0,0 +1,52 @@
+import time
+from asyncio import wait
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.devtools.v141.dom_storage import Item
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+
+service = Service("D:/OneDrive/Learning/Python_Selenium/chromedriver-win64/chromedriver.exe")
+try:
+    driver = webdriver.Chrome(
+        service=service,
+        options=chrome_options
+    )
+    driver.implicitly_wait(5)
+    driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+    driver.find_element(By.CSS_SELECTOR, "input.search-keyword").send_keys("ber")
+    time.sleep(3)
+    item_list = driver.find_elements(By.CSS_SELECTOR, "div.products-wrapper .products .product")
+    assert len(item_list) > 0
+
+    for item in item_list:
+        item.find_element(By.CSS_SELECTOR, ".product-action button").click()
+
+    driver.find_element(By.CSS_SELECTOR, ".cart-icon img").click()
+    driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+    driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys("rahulshettyacademy")
+    driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+    WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.CSS_SELECTOR, ".promoInfo")) )
+
+    cart_items = driver.find_elements(By.CSS_SELECTOR, "#productCartTables td:nth-last-child(1) .amount")
+    total_expected = 0
+    for item in cart_items:
+        item.text
+        total_expected += int(item.text)
+    total_actual = int(driver.find_element(By.CSS_SELECTOR, ".totAmt").text)
+    assert total_expected == total_actual
+
+    discount_percent = int(driver.find_element(By.CSS_SELECTOR, ".discountPerc").text.replace("%", "")) / 100
+    total_expected_after_discount = total_expected * (1 - discount_percent)
+    total_actual_after_discount = total_actual * (1 - discount_percent)
+    assert total_expected_after_discount == total_actual_after_discount
+
+finally:
+    driver.quit()
